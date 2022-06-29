5a64fb71f697545c9ac55028299333191802e949
katesheelee <katerinashelepil@gmail.com>
2022-06-29 20:08:37 +0300

    update the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+function formatDate(timestamp) {
+	let now = new Date(timestamp);
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
+	let day = now.getDay();
+
+	let days = [
+		"#",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+		"Sunday",
+	];
+	return `${days[day]} ${hours}:${minutes}`;
+}
+
 function displayTemperature(responce) {
 	let temperatureElement = document.querySelector("#temp");
 	temperatureElement.innerHTML = Math.round(responce.data.main.temp);
@@ -9,7 +28,10 @@ function displayTemperature(responce) {
 	humidityElement.innerHTML = responce.data.main.humidity;
 	let windElement = document.querySelector("#wind");
 	windElement.innerHTML = Math.round(responce.data.wind.speed);
+	let dateElement = document.querySelector("#date");
+	dateElement.innerHTML = formatDate(responce.data.dt * 1000);
 }
+
 let apiKey = "4be4c5e07e7604cd26d0eb3ec1d11faf";
 let city = "Paris";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
